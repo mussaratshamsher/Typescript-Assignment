d06a11216ab679a467dfa984c009b2a65be97195
mussaratshamsher <mussaratskhan@github.com>
2024-03-17 07:55:30 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,44 @@
+ // Exe 2
+let personName = "Eric";
+console.log(`Hello, ${personName} would you like to learn some Pythan today?`);
+
+// Exe 3
+//lower case
+let personname: string="Babar"
+console.log("lowercase:", personname.toLowerCase());
+//upper case
+console.log("upper case:", personname.toLocaleUpperCase());
+//title case
+console.log("titlecase:", personname.replace(/\bw/g,c => c.toUpperCase()));
+
+// Exe 4
+console.log('Albert Einstein once said, "A person who never made a mistake never tried anything new."');
+
+// Exe 5
+let famousPerson = "Albert Einstien";
+let message = `${famousPerson} once said, "A personwho mever made a mistake never tried anything new."`;
+console.log(message);
+
+// Exe 6
+let person_name = "\n\t BABAR AZAM\t\n";
+console.log(person_name);
+let stripped: string= person_name.trim();
+console.log(stripped);
+// Exe 7,8
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
+
+// Exe 9
+let favouriteNumber: number = 7;
+console.log(`My favourite number is ${favouriteNumber}`);
+
+//Exe 1o
+// name: Mussarat , 29-03-2024
+let personName1 = "Mussarat";
+// Message 
+console.log(`${personName1} says, \"Time does not change people, time reveals true faces of the people."`);
+
+
+
